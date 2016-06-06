d41aec94e41720011d5d2224d887b419fd364c89
romangrb <grabarroman@gmail.com>
2016-06-06 13:53:44 +0000

    provide templete remind password and register

--- app/app_main.js
+++ app/app_main.js
@@ -7,6 +7,16 @@ function config($routeProvider){
       controller:'registerApp.loginController',
       controllerAs:'vm'
     })
+    .when('/register',{
+      templateUrl:'templates/register.html',
+      controller:'registerApp.loginController',
+      controllerAs:'vm'
+    })
+    .when('/remind',{
+      templateUrl:'templates/remind_password.html',
+      controller:'registerApp.loginController',
+      controllerAs:'vm'
+    })
     .otherwise({redirectTo:'/'});
 }
 

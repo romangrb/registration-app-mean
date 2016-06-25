eb9f594df40a7065a98e08ee3b1286046a02ed90
romangrb <grabarroman@gmail.com>
2016-06-25 13:20:07 +0000

    fixed expresss-connect usage

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 var express = require('express'),
   mongoose = require('mongoose'),
   bodyParser = require('body-parser'),
-  app = express(),
   session = require('express-session'),
+  MongoStore = require('connect-mongo')(session),
+  app = express(),
   config = require('config'),
   dbConfig = config.get('Customer.dbConfig'),
   
   IP = process.env.IP,
   PORT = process.env.PORT;
 
-  //mongoose.connect(dbConfig.url, dbConfig.options);
+  mongoose.connect(dbConfig.url, dbConfig.options);
 
   app.use(bodyParser.urlencoded({
       extended: true
@@ -17,15 +18,22 @@ var express = require('express'),
   
   app.use(express.static('.'));
   
-  app.get('/', function (request, response) {
-    response.sendfile('index.html');
+  app.use(session({
+      secret: config.get('Customer.session.secret'),
+      key: config.get('Customer.session.key'),
+      cookie: config.get('Customer.session.cookie'),
+      store: new MongoStore()
+  }));
+  
+  app.get('/', function (req, res, next) {
+      res.sendfile('index.html');
   });
   
   app.listen(PORT, IP, 
-  
-  function(){
-      console.log("The server is run  on \n port :", PORT, "\n ip", IP);
-  },
-  function(err){
-      console.log("Can't run server at :", PORT, "\n ip", IP, "\n ERROR : ", err);
-  });
+      function(){
+          console.log("The server is run  on \n port :", PORT, "\n ip", IP);
+      },
+      function(err){
+          console.log("Can't run server at :", PORT, "\n ip", IP, "\n ERROR : ", err);
+      }
+  );

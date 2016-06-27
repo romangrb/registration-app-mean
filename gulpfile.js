08011026a65deeaced629c3de5e09d211dd94c5b
romangrb <grabarroman@gmail.com>
2016-06-27 23:23:04 +0000

    change gulp version to 4 change dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,8 @@
-/*var gulp = require('gulp'),
-    server = require('gulp-express');
-
-gulp.task('server', function () {
-    return server.run({
-        file: 'app.js'
-    });
-});
-
-gulp.task('default', ['server']);
-*/
 
 const gulp = require('gulp');
 const spawn = require('child_process').spawn;
+const browserSync = require('browser-sync').create();
+const gutil = require('gulp-util');
 var node;
 
 /**
@@ -19,13 +10,56 @@ var node;
  * description: launch the server. If there's a server already running, kill it.
  */
 gulp.task('server', function() {
+
   if (node) node.kill()
   node = spawn('node', ['app.js'], {stdio: 'inherit'})
   node.on('close', function (code) {
     if (code === 8) {
-      gulp.log('Error detected, waiting for changes...');
+      gutil.log('Error detected, waiting for changes...');
     }
   });
+});
+
+// Static server
+
+gulp.task('frontEndReload', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    
+    browserSync.watch(
+                   [
+                      './app.js', 
+                      './app/**/*.js', 
+                      './config/*.*', 
+                      './model/*.*',
+                      './index.html',
+                    ],
+                    {},
+                    browserSync.reload
+    )//.on('change', browserSync.reload);
+                     
+});
+
+gulp.task('backEndReload', function() {
+    gulp.watch(
+            [
+              './app.js', 
+              './app/**/*.js', 
+              './config/*.*', 
+              './model/*.*',
+              './index.html',
+            ],
+            {},
+            gulp.series(gulp.parallel('server', 'backEndReload', 'frontEndReload'))
+    )
+});
+
+// clean up if an error goes unhandled.
+process.on('exit', function() {
+    if (node) node.kill()
 })
 
 /**
@@ -33,22 +67,5 @@ gulp.task('server', function() {
  * description: start the development environment
  */
  
-gulp.task('default', function() {
-  gulp.run('server')
+gulp.task('default', gulp.series(gulp.parallel('server', 'backEndReload', 'frontEndReload')))//, 'frontEndReload'));
 
-  gulp.watch([
-      './app.js', 
-      './app/**/*.js', 
-      './config/*.*', 
-      './model/*.*',
-      './index.html',
-      ], function() {
-    gulp.run('server')
-  })
-  
-})
-
-// clean up if an error goes unhandled.
-process.on('exit', function() {
-    if (node) node.kill()
-})
